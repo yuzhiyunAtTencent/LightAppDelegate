40e24103c286d00f8f55d76518067fd957821a74
zhiyunyu <zhiyunyu@tencent.com>
2018-11-07 18:01:51 +0800

    Initial Commit

---
+++ LightAppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightAppDelegate
+//
+//  Created by zhiyunyu on 2018/11/7.
+//  Copyright © 2018 zhiyunyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightAppDelegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightAppDelegate
+//
+//  Created by zhiyunyu on 2018/11/7.
+//  Copyright © 2018 zhiyunyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
