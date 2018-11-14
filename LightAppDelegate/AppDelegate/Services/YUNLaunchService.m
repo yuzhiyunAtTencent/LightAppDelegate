8106da5955129fad7c8bb3cf201e591801247812
zhiyunyu <zhiyunyu@tencent.com>
2018-11-14 17:07:04 +0800

    添加app的根视图

--- LightAppDelegate/AppDelegate/Services/YUNLaunchService.m
+++ LightAppDelegate/AppDelegate/Services/YUNLaunchService.m
@@ -15,6 +15,14 @@
     // Override point for customization after application launch.
     NSLog(@"AppDelegate ************ App has launched");
     
+    // 如果在appdelegate文件中要获取window,直接用self.window即可。
+    [UIApplication sharedApplication].delegate.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]]; //设置窗口
+
+    UIViewController *mainVC = [[RootViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:mainVC];
+    [UIApplication sharedApplication].delegate.window.rootViewController = nav; //进入的首个页面
+    [[UIApplication sharedApplication].delegate.window makeKeyAndVisible]; //显示
+
     return YES;
 }
 
